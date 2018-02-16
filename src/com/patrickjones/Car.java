2424a1181a63ddd8407348785ababae560018ffb
patrickatcw <pj8392@yahoo.com>
2018-02-15 20:45:45 -0500

    constructors, model and color completed

--- src/com/patrickjones/Car.java
+++ src/com/patrickjones/Car.java
@@ -1,4 +1,30 @@
 package com.patrickjones;
 
 public class Car {
+
+    private int doors;
+    private int wheels;
+    private int year;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model, String color) {
+       /* String validModel = model.toLowerCase();
+        if(validModel.equals("charger") || validModel.equals("cobalt") ||validModel.equals("focus") ){
+        }else{
+            this.model = "Unknown";
+        }*/
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
 }
--- src/com/patrickjones/Main.java
+++ src/com/patrickjones/Main.java
@@ -1,4 +1,22 @@
 package com.patrickjones;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Car dodge = new Car();
+        Car chevy = new Car();
+        Car ford = new Car();
+
+        dodge.setModel("Charger", "red");
+        chevy.setModel("Cobalt", "blue");
+        ford.setModel("Focus", "white");
+
+        System.out.println("Car model is a " + dodge.getColor() + " Dodge " + dodge.getModel());
+        System.out.println("Car model is a " + chevy.getColor() + " Chevy " + chevy.getModel());
+        System.out.println("Car model is a " + ford.getColor() + " Ford " + ford.getModel());
+
+    }
 }
+
+
