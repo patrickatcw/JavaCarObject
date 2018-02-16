5323c1016111952b7f01bd243fea073d79ba93fc
patrickatcw <pj8392@yahoo.com>
2018-02-16 11:32:55 -0500

    did some code clean-up and additional comments for information, or reference, purposes

--- src/com/patrickjones/Car.java
+++ src/com/patrickjones/Car.java
@@ -1,37 +1,24 @@
 package com.patrickjones;
 
+//object is an instance of a class
+//java class types; public, private, protected
+
 public class Car {              //class Car
 
-    private int doors;              //fields
-    private int wheels;
-    private int year;
-    private String model;
-    private String engine;
+    private String model;       //fields
     private String color;
+    private int year;
+    private String engine;
+    private int doors;
     private String transmission;
 
+    //public Car() {           constructor, or default constructor, existing without me putting it there,
+       // }                    but....i made my own below with this. for references to fields above
 
-   //public Car() {           //constructor, or default constructor, existing without me putting it there
+    //made my own constructor (method) with parameters and references to fields above.
+    public Car(String model, String color, int year, String engine, int doors, String transmission) {
 
-   // }
-
-    public Car(int doors, int wheels, int year, String model, String engine, String color, String transmission) {
-        this.doors = doors;
-        this.wheels = wheels;
-        this.year = year;
         this.model = model;
-        this.engine = engine;
-        this.color = color;
-        this.transmission = transmission;
-    }
-
-    public void carInfo(String model, String color, int year, String engine, int doors, String transmission) {      //carInfo method
-       /* String validModel = model.toLowerCase();
-        if(validModel.equals("charger") || validModel.equals("cobalt") ||validModel.equals("focus") ){
-        }else{
-            this.model = "Unknown";
-        }*/
-        this.model = model;     //this. refers to fields and (updating model, = model is parameter), assigning
         this.color = color;
         this.year = year;
         this.engine = engine;
--- src/com/patrickjones/Main.java
+++ src/com/patrickjones/Main.java
@@ -1,41 +1,46 @@
 package com.patrickjones;
 
+//object is an instance of a class
+//java class types; public, private, protected
+
 public class Main {                         //class Main
 
     public static void main(String[] args) {        //main method
 
-        Car dodge = new Car();          //using constructors to build a new instance
-        Car chevy = new Car();
-        Car ford = new Car();
-        Car toyota = new Car();
+        //using constructors to build a new instance
+        Car dodge = new Car("Charger", "red", 1968, "powerful", 2, "manual");
+        Car chevy = new Car("Cobalt", "blue", 1998, "small", 2, "automatic");
+        Car ford = new Car("Focus", "white", 2012, "tiny", 4, "automatic");
+        Car toyota = new Car("Camry", "silver", 2010, "mid-size", 2, "manual");
 
-        dodge.carInfo("Charger", "red", 1968, "powerful", 2, "manual");
-        chevy.carInfo("Cobalt", "blue", 1998, "small", 2, "automatic");
-        ford.carInfo("Focus", "white", 2012, "tiny", 4, "automatic");
-        toyota.carInfo("Camry", "silver", 2010, "mid-size", 2, "manual");
-
-        System.out.println("The car is a " + dodge.getColor()   //complete transmission
+        System.out.println("The car is a " + dodge.getColor()
                 + " " + dodge.getYear() + " Dodge " + dodge.getModel()
                 + " with a " + dodge.getEngine() + " engine, " + dodge.getDoors()
                 + " doors, and a " + dodge.getTransmission() + " transmission.");
         System.out.println("The car is a " + chevy.getColor()
                 + " " + chevy.getYear() + " Chevy " + chevy.getModel()
-                + " with a " + chevy.getEngine() + " engine and " + chevy.getDoors()
-                + " doors.");
+                + " with a " + chevy.getEngine() + " engine, " + chevy.getDoors()
+                + " doors, and an " + chevy.getTransmission() + " transmission.");
         System.out.println("The car is a " + ford.getColor()
                 + " " + ford.getYear() + " Ford " + ford.getModel()
-                + " with a " + ford.getEngine() + " engine and " + ford.getDoors()
-                + " doors.");
+                + " with a " + ford.getEngine() + " engine, " + ford.getDoors()
+                + " doors, and an " + ford.getTransmission() + " transmission.");
         System.out.println("The car is a " + toyota.getColor()
                 + " " + toyota.getYear() + " Toyota " + toyota.getModel()
-                + " with a " + toyota.getEngine() + " engine and " + toyota.getDoors()
-                + " doors.");
-
+                + " with a " + toyota.getEngine() + " engine, " + toyota.getDoors()
+                + " doors, and a " + toyota.getTransmission() + " transmission.");
 
     }
 }
 
-//object is an instance of a class
-//java class types; public, private, protected
+/*
+printout results;
+    The car is a red 1968 Dodge Charger with a powerful engine, 2 doors, and a manual transmission.
+    The car is a blue 1998 Chevy Cobalt with a small engine, 2 doors, and an automatic transmission.
+    The car is a white 2012 Ford Focus with a tiny engine, 4 doors, and an automatic transmission.
+    The car is a silver 2010 Toyota Camry with a mid-size engine, 2 doors, and a manual transmission.
+*/
+
+
 
 
